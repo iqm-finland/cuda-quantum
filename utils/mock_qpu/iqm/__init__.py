6fcaaf836fbc4708d6189a513b0a133db8d82fd0
iqm-bhoffmann <bernd.hoffmann@iqm.tech>
2026-08-12 13:11:51 +0000

    Testcases for split topology algorithm
    Added a test for the split topology algorithm. The tests are based on verified testvectors for the 20 qubit and 54 qubit architectures.
    The mock server for IQM was extended to be configurable at runtime to support different configurations in the same testrun.
    [IQM-SW-2740]
    
    Signed-off-by: iqm-bhoffmann <bernd.hoffmann@iqm.tech>

--- utils/mock_qpu/iqm/__init__.py
+++ utils/mock_qpu/iqm/__init__.py
@@ -29,7 +29,8 @@ To simulate a QPU with an imperfect calibration this list can be used to
 deliberately exclude `prx` gates from the dynamic quantum architecture.
 By default 2 qubits are excluded which the integration needs to skip over
 in order for tests to succeed. This list can be set at startup by assigning
-a list to the environment variable IQM_MOCK_BAD_PRX_GATES.
+a list to the environment variable IQM_MOCK_BAD_PRX_GATES. It can also be
+extended dynamically at runtime using HTTP requests to endpoints defined below.
 """
 
 bad_cz_gates = []
@@ -37,47 +38,13 @@ bad_cz_gates = []
 Similar to `bad_qubits_prx` this lists all the CZ gates which should be removed
 from the dynamic quantum architecture to simulate an imperfect calibration.
 This list can be set at startup by assigning a list to the environment variable
-IQM_MOCK_BAD_CZ_GATES.
+IQM_MOCK_BAD_CZ_GATES. It can also be extended dynamically at runtime using
+HTTP requests to endpoints defined below.
 """
 
-qubits = [
-    "QB1", "QB2", "QB3", "QB4", "QB5", "QB6", "QB7", "QB8", "QB9", "QB10",
-    "QB11", "QB12", "QB13", "QB14", "QB15", "QB16", "QB17", "QB18", "QB19",
-    "QB20"
-]
+qubits = []
 
-qubit_connectivity = [
-    ["QB1", "QB2"],
-    ["QB1", "QB4"],
-    ["QB2", "QB5"],
-    ["QB3", "QB4"],
-    ["QB3", "QB8"],
-    ["QB4", "QB5"],
-    ["QB4", "QB9"],
-    ["QB5", "QB6"],
-    ["QB5", "QB10"],
-    ["QB6", "QB7"],
-    ["QB6", "QB11"],
-    ["QB7", "QB12"],
-    ["QB8", "QB9"],
-    ["QB8", "QB13"],
-    ["QB9", "QB10"],
-    ["QB9", "QB14"],
-    ["QB10", "QB11"],
-    ["QB10", "QB15"],
-    ["QB11", "QB12"],
-    ["QB11", "QB16"],
-    ["QB12", "QB17"],
-    ["QB13", "QB14"],
-    ["QB14", "QB15"],
-    ["QB14", "QB18"],
-    ["QB15", "QB16"],
-    ["QB15", "QB19"],
-    ["QB16", "QB17"],
-    ["QB16", "QB20"],
-    ["QB18", "QB19"],
-    ["QB19", "QB20"],
-]
+qubit_connectivity = []
 
 computational_resonators = []
 
@@ -97,41 +64,115 @@ async def lifespan(app: FastAPI):
        Example: `export IQM_MOCK_BAD_CZ_GATES="QB1-QB2,QB5-QB6,QB19-QB20"`
     """
 
+    # default quantum architecture on startup
+    _generate_quantum_architecture("crystal-20")
+
     if ("IQM_MOCK_BAD_PRX_GATES" in environ):
         # Allow setting the list of bad PRX gates via environment variable.
         bad_qubits_prx.clear()
         gate_list = environ["IQM_MOCK_BAD_PRX_GATES"]
-        for qb in gate_list.split(","):
-            if qb.strip() in qubits:
-                bad_qubits_prx.append(qb.strip())
+        _parse_bad_prx_gate_list(gate_list)
 
     for qb in bad_qubits_prx:
-        print(f"No prx-gate possible on: {qb}")
+        print(f"Disabled PRX-gate on: {qb}")
 
     if ("IQM_MOCK_BAD_CZ_GATES" in environ):
         # Likewise a list of bad CZ gates can be given.
         bad_cz_gates.clear()
         gate_list = environ["IQM_MOCK_BAD_CZ_GATES"]
-        for gate in gate_list.split(","):
-            cz_qubits: list[str] = gate.split("-")
-            if len(cz_qubits) == 2:
-                bad_cz_gates.append(cz_qubits)
-        for gate in bad_cz_gates:
-            if gate in qubit_connectivity:
-                qubit_connectivity.remove(gate)
-                print(f"No cz-gate possible between: {gate[0]}-{gate[1]}")
-            else:
-                reverse_gate: list[str] = gate.copy()
-                reverse_gate.reverse()
-                if reverse_gate in qubit_connectivity:
-                    qubit_connectivity.remove(reverse_gate)
-                    print(f"No cz-gate possible between: "
-                          f"{reverse_gate[0]}-{reverse_gate[1]}")
+        _parse_bad_cz_gate_list(gate_list)
+
+    _process_bad_cz_gate_list()
+
     yield
 
 
 # Define the REST Server App
 app = FastAPI(title="IQM Mock QPU Server", version="V1", lifespan=lifespan)
+
+
+def _parse_bad_prx_gate_list(gate_list: str):
+    """Parse the given list and update the global var bad_qubits_prx."""
+    for qb in gate_list.split(","):
+        qb = qb.strip()
+        if qb in qubits and qb not in bad_qubits_prx:
+            bad_qubits_prx.append(qb)
+
+
+def _parse_bad_cz_gate_list(gate_list: str):
+    """Parse the given list and update the global var bad_cz_gates."""
+    for gate in gate_list.split(","):
+        cz_qubits: list[str] = gate.split("-")
+        if len(cz_qubits) == 2 and cz_qubits not in bad_cz_gates:
+            bad_cz_gates.append(cz_qubits)
+
+
+def _process_bad_cz_gate_list():
+    """Process the global var bad_cz_gates and remove the listed cz-gates from
+    the qubit connectivity list."""
+    for gate in bad_cz_gates:
+        if gate in qubit_connectivity:
+            qubit_connectivity.remove(gate)
+            print(f"Disabled CZ-gate between: {gate[0]}-{gate[1]}")
+        else:
+            reverse_gate: list[str] = gate.copy()
+            reverse_gate.reverse()
+            if reverse_gate in qubit_connectivity:
+                qubit_connectivity.remove(reverse_gate)
+                print(f"Disabled CZ-gate between: "
+                        f"{reverse_gate[0]}-{reverse_gate[1]}")
+
+
+def _generate_quantum_architecture(qpu: str) -> bool:
+    """Generate the quantum architecture description of the specified QPU.
+    This populates the global variables 'qubits' and 'qubit_connectivity'."""
+    architectures = {
+        "crystal-5":  [1, 0, 3, 1, 1, -1],
+        "crystal-20": [2, 0, 5, 1, 5, 0, 5, 0, 3, -1],
+        "crystal-54": [2, 0, 5, 2, 7, 1, 8, 1, 9, 0, 8, -1, 7, 0, 5, -1, 3, -1]
+    }
+    if qpu not in architectures:
+        return False
+
+    qubits.clear()
+    qubit_connectivity.clear()
+    layout = architectures[qpu]
+
+    # generate the list of two qubit gates
+    row_start = 1
+    last_row_start = 0
+    last_row_length = 0
+
+    for r in range(0, len(layout), 2):
+        row_length = layout[r]
+        row_offset = layout[r + 1]
+
+        # horizontal
+        for qb in range(0, row_length - 1):
+            qb1 = row_start + qb
+            qb2 = qb1 + 1
+            qubit_connectivity.append([f"QB{qb1}", f"QB{qb2}"])
+
+        # vertical
+        if last_row_start != 0:
+            for qb in range(0, min(last_row_length, row_length)):
+                if row_offset >= 0:
+                    qb1 = last_row_start + qb
+                    qb2 = row_start + row_offset + qb
+                else:
+                    qb1 = last_row_start - row_offset + qb
+                    qb2 = row_start + qb
+                qubit_connectivity.append([f"QB{qb1}", f"QB{qb2}"])
+
+        last_row_length = row_length
+        last_row_start = row_start
+        row_start += row_length
+
+    # generate the list of qubits
+    qubit_cnt = row_start - 1
+    qubits.extend(f"QB{qb + 1}" for qb in range(qubit_cnt))
+
+    return True
 
 
 class Counts(BaseModel):
@@ -477,15 +518,10 @@ async def get_job_status(job_id: str, request: Request):
 
     results = {
         # Note: this is a subset of what a real server would return.
-        "messages": [
-            {
-                "message":
-                    job.result.message
-                    if job.result and job.result.message else None,
-                "source":
-                    "iqm-server"
-            }
-        ],
+        "artifacts":
+            [],
+        "messages":
+            [],
         "queue_position":
             1,
         "runtime_ms":
@@ -493,6 +529,12 @@ async def get_job_status(job_id: str, request: Request):
         "status":
             "completed" if job.status == iqm_client.Status.READY else job.status
     }
+
+    if job.status == iqm_client.Status.FAILED:
+        results["errors"] = list()
+        results["errors"].append({"error_code": "unknown",
+                                  "message": job.result.message,
+                                  "source": "iqm-server"})
 
     return results
 
@@ -513,3 +555,46 @@ async def get_job_counts(job_id: str, request: Request):
     results = job.counts_batch
 
     return results
+
+
+@app.get("/config/qa/qpu")
+async def set_qa_qpu(qpu: str, request: Request):
+    """Set the quantum architecture by selecting a QPU"""
+    access_token = request.headers.get("Authorization")
+    if access_token != good_access_token:
+        raise HTTPException(401)
+
+    status = _generate_quantum_architecture(qpu)
+    if not status:
+        raise HTTPException(404, "Requested QPU not found")
+
+    bad_qubits_prx.clear()
+    bad_cz_gates.clear()
+
+    print(f"Using QPU architecture {qpu} now.")
+    return {"message": "ok"}
+
+
+@app.get("/config/qa/bad-prx-gates")
+async def set_qa_bad_qubits_prx(loci_list: str, request: Request):
+    """Set a list of qubits which cannot be used for PRX gates."""
+    access_token = request.headers.get("Authorization")
+    if access_token != good_access_token:
+        raise HTTPException(401)
+
+    _parse_bad_prx_gate_list(loci_list)
+    for qb in bad_qubits_prx:
+        print(f"Disabled PRX-gate on: {qb}")
+    return {"message": "ok"}
+
+
+@app.get("/config/qa/bad-cz-gates")
+async def set_qa_bad_cz_gates(loci_list: str, request: Request):
+    """Set a list of cz-gates which cannot be used."""
+    access_token = request.headers.get("Authorization")
+    if access_token != good_access_token:
+        raise HTTPException(401)
+
+    _parse_bad_cz_gate_list(loci_list)
+    _process_bad_cz_gate_list()
+    return {"message": "ok"}
